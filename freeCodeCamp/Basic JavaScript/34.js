71db336c9c08407142be231e4fd75278ce8207b8
MariannaPo <mapodmasko@gmail.com>
2022-09-15 17:13:39 +0300

    34-43

---
+++ freeCodeCamp/Basic JavaScript/34.js
@@ -0,0 +1,5 @@
+
+let myStr = "Jello World";
+
+
+myStr = "Hello World";
---
+++ freeCodeCamp/Basic JavaScript/38.js
@@ -0,0 +1,6 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks = "Scary " +myAdjective+" shaggy "+myNoun+" fast "+myVerb+" to me and i go "+myAdverb;
